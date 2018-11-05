2bd476d4cd6a8b2f1188deea1038dd2d792a658d
jindalshiva <jindalshivani11@gmail.com>
2018-11-05 18:01:07 +0530

    RecylerView is working

--- app/src/main/java/com/example/nandkishorjindal/recyclerviewexample/MainActivity.java
+++ app/src/main/java/com/example/nandkishorjindal/recyclerviewexample/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.nandkishorjindal.recyclerviewexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+
+    private List<ListItem> listItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        recyclerView = (RecyclerView) findViewById( R.id.recyclerView );
+        recyclerView.setHasFixedSize( true );
+        recyclerView.setLayoutManager( new LinearLayoutManager( this ) );
+
+        listItems = new ArrayList<>(  ) ;
+
+        for (int i=0; i<10;i++){
+            ListItem listItem = new ListItem(
+                    "heading " + (i+1),
+                    "Lorem lypsun dummy text"
+            );
+            listItems.add( listItem );
+
+
+        }
+        adapter = new MyAdapter( listItems,this );
+        recyclerView.setAdapter( adapter ) ;
     }
 }
